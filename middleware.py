c68675218cc93b7433fd7c3aad5b695fe9e7faf2
Nguyen Van Ngoc <ngocnv@Nguyens-MacBook-Pro.local>
2020-09-11 16:07:59 +0700

    extract request body

--- middleware.py
+++ middleware.py
@@ -1,6 +1,9 @@
 from tyk.decorators import *
 from gateway import TykGateway as tyk
 from time import time
+from google.protobuf.json_format import MessageToDict
+import json
+
 
 @Hook
 def PreHook(request, session, spec):
@@ -17,8 +20,22 @@ def PostHook(request, session, spec):
 @Hook
 def AuthCheck(request, session, metadata, spec):
     tyk.log("AuthCheck is called", "info")
+    return request, session, metadata
 
-    # request.get_header is a helper method, to get the full header list, use request.object.headers
+@Hook
+def PostKeyAuth(request, session, metadata, spec):
+    tyk.log("PostKeyAuth is called", "info")
+    tyk_request = MessageToDict(request.__dict__['object'])
+    req_body = json.loads(tyk_request['body'])
+    tyk.log("PostKeyAuth: request body info: {0}".format(req_body), "info")
+    tyk.log("PostKeyAuth: req body type: {0}".format(type(req_body)), "info")
+    tyk.log("PostKeyAuth: username: {0}".format(req_body['name']), "info")
+
+
+    # Log the additional metadata (set in AuthCheck):
+    username = session.metadata["username"]
+    tyk.log("PostKeyAuth: user '{0}' was authenticated".format(username), "info")
+
     auth_header = request.get_header('Authorization')
     if auth_header == '47a0c79c427728b3df4af62b9228c8ae':
         tyk.log("AuthCheck is successful", "info")
@@ -39,15 +56,6 @@ def AuthCheck(request, session, metadata, spec):
     request.object.return_overrides.response_error = 'Invalid authentication'
     request.object.return_overrides.response_code = 403
     return request, session, metadata
-
-@Hook
-def PostKeyAuth(request, session, spec):
-    tyk.log("PostKeyAuth is called", "info")
-
-    # Log the additional metadata (set in AuthCheck):
-    username = session.metadata["username"]
-    tyk.log("PostKeyAuth: user '{0}' was authenticated".format(username), "info")
-    return request, session
 
 @Hook
 def ResponseHook(request, response, session, metadata, spec):
